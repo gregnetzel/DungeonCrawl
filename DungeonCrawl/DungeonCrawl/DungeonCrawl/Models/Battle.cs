28e8ae1ede42f484cb0c03294dba14a0fe38a38e
gregnetzel <greg.8535@gmail.com>
2017-04-25 10:40:36 -0700

    Add Battle Class

---
+++ DungeonCrawl/DungeonCrawl/DungeonCrawl/Models/Battle.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DungeonCrawl.Models
+{
+    class Battle
+    {
+        struct MonsterParty
+        {
+            public List<Monster> party;
+            public void GenerateMonsters()
+            {
+                party.Add(new Monster());
+                //todo
+            }
+            public bool AllDead()
+            {
+                //todo
+                return true;
+            }
+        }
+        struct PlayerParty
+        {
+            public List<Player> party;
+            public bool AllDead()
+            {
+                //todo
+                return true;
+            }
+        }
+        public Battle(Player[] p)
+        {
+            MonsterParty monsters = new MonsterParty();
+            PlayerParty players = new PlayerParty();
+            players.party.AddRange(p);
+            monsters.GenerateMonsters();
+            monsters.party.Sort();
+        }
+    }
+}
