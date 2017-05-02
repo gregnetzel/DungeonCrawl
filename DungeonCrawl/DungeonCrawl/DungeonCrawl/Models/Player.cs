794a2ca76125c9cbc5049078a00ffb1b6264b703
Alvin Nawa <alvinnawa11@gmail.com>
2017-05-02 09:34:16 -0700

    Updated Player Model (GetItem) & Player Test

--- DungeonCrawl/DungeonCrawl/DungeonCrawl/Models/Player.cs
+++ DungeonCrawl/DungeonCrawl/DungeonCrawl/Models/Player.cs
@@ -1,47 +1,81 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Text;
+using System.Linq;
 
-namespace DungeonCrawl.Models
+namespace MobileApplication
 {
-    public class Player:Character
-    {
-        public int CurrentXP { get; set; }
-        public ObservableCollection<Item> CurrentItems { get; set; } // What's inventory for then?
+	public class Player : Characters
+	{
+		public int CurrentXP { get; set; }
+        public ObservableCollection<Items> CurrentItems;
 
-        public void AddXP(int XP)
+        public Player()
         {
-            CurrentXP += XP;
+            CurrentItems = new ObservableCollection<Items>();
         }
 
-        public void LevelUp()
-        {
-            if (CurrentXP >= 100) Level++;
-            CurrentXP = 0;
+		public void AddXP(int XP)
+		{
+			CurrentXP += XP;
+			LevelUp();
+		}
 
-            if (Level % 2 == 0)
-            {
-                Str++;
-                Spd++;
-            }
-            else if (Level % 3 == 0)
-            {
-                Dex++;
-                HP += 3;
-            }
-            else if (Level % 10 == 0)
-            {
-                Str++;
-                Spd++;
-                Dex++;
-                HP += 5;
-            }
-        }
+		public void LevelUp()
+		{
+			if (CurrentXP >= 100)
+			{
+				Level++;
+				CurrentXP = 0;
 
-        public void GetItem(Item item)
-        {
-            CurrentItems.Add(item);
-        }
-    }
+				if (Level % 2 == 0)
+				{
+					Str++;
+					Spd++;
+				}
+				else if (Level % 3 == 0)
+				{
+					Dex++;
+					HP += 3;
+				}
+				else if (Level % 10 == 0)
+				{
+					Str++;
+					Spd++;
+					Dex++;
+					HP += 5;
+				}
+				else HP += 2;
+			}
+		}
+
+		public void GetItem(Items Item)
+		{
+            var item = CurrentItems.FirstOrDefault(i => i.InvLoc == Item.InvLoc);
+            if (item == null) // Item does not exist
+            {
+                CurrentItems.Insert(Item.InvLoc, Item);
+				this.Str += Item.StrValue;
+				this.Dex += Item.DexValue;
+				this.Spd += Item.SpdValue;
+				this.HP += Item.HPValue;
+            }
+            else
+            {
+                // Compare the two items
+				if (CurrentItems[Item.InvLoc].StrValue < Item.StrValue ||
+					CurrentItems[Item.InvLoc].DexValue < Item.DexValue ||
+					CurrentItems[Item.InvLoc].SpdValue < Item.SpdValue ||
+					CurrentItems[Item.InvLoc].HPValue < Item.HPValue)
+				{
+					// If CurrentItem is worse than new Item
+					this.Str += (Item.StrValue - CurrentItems[Item.InvLoc].StrValue);
+					this.Dex += (Item.DexValue - CurrentItems[Item.InvLoc].DexValue);
+					this.Spd += (Item.SpdValue - CurrentItems[Item.InvLoc].SpdValue);
+					this.HP += (Item.HPValue - CurrentItems[Item.InvLoc].HPValue);
+                    CurrentItems.RemoveAt(Item.InvLoc);
+					CurrentItems.Insert(Item.InvLoc, Item); // Replace existing item
+				}
+            }
+		}
+	}
 }
--- DungeonCrawl/UnitTestProject1/PlayerTest.cs
+++ DungeonCrawl/UnitTestProject1/PlayerTest.cs
@@ -1,27 +1,88 @@
-﻿using System;
-using System.Text;
-using System.Collections.Generic;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-namespace UnitTestProject1
+﻿using NUnit.Framework;
+using System;
+namespace MobileApplication.Test
 {
-    /// <summary>
-    /// Summary description for PlayerTest
-    /// </summary>
-    [TestClass]
+    [TestFixture()]
     public class PlayerTest
     {
-        public PlayerTest()
-        {
-            //
-            // TODO: Add constructor logic here
-            //
-        }
+		[Test]
+		public void TestIsDead()
+		{
+            var myPlayer = new Player();
 
-        [TestMethod]
-        public void PlayerTestDeath()
-        {
-            
-        }
+            myPlayer.HP = 0;
+
+            Assert.IsTrue(myPlayer.IsDead(), "IsDead has errors");
+		}
+
+		[Test]
+		public void TestIsAlive()
+		{
+            var myPlayer = new Player();
+
+            myPlayer.HP = 2;
+
+            Assert.IsFalse(myPlayer.IsDead(), "IsDead has errors");
+		}
+
+		[Test]
+		public void TestAddXP()
+		{
+            var myPlayer = new Player();
+
+            myPlayer.CurrentXP = 0;
+            myPlayer.AddXP(50);
+
+            Assert.IsTrue(myPlayer.CurrentXP == 50, "AddXP has errors");
+		}
+
+		[Test]
+		public void TestLevelUp()
+		{
+            var myPlayer = new Player();
+            myPlayer.CurrentXP = 0;
+            myPlayer.Level = 1;
+
+            myPlayer.AddXP(100);
+
+            Assert.IsTrue(myPlayer.Level == 2, "LevelUp has errors");
+		}
+
+		[Test]
+		public void TestAddItemsAdded()
+		{
+            var myPlayer = new Player { Str = 2 };
+            Items tempItem = new Items { Name = "Test", StrValue = 2, InvLoc = 0};
+
+            myPlayer.GetItem(tempItem);
+
+            Assert.IsTrue(myPlayer.CurrentItems.Count > 0 && myPlayer.Str == 4, "GetItems has errors");
+		}
+
+		[Test]
+		public void TestAddItemsRejected()
+		{
+            var myPlayer = new Player { Str = 2 };
+            Items tempItem = new Items { Name = "Test", StrValue = 3, InvLoc = 0};
+            Items tempItem2 = new Items { Name = "Test1", StrValue = 2, InvLoc = 0 };   
+
+			myPlayer.GetItem(tempItem);
+            myPlayer.GetItem(tempItem2);
+
+            Assert.IsTrue(myPlayer.CurrentItems[0].Name == "Test" && myPlayer.Str == 5, "GetItems has errors");
+		}
+
+		[Test]
+		public void TestAddItemsReplaced()
+		{
+			var myPlayer = new Player { Str = 2 };
+			Items tempItem = new Items { Name = "Test", StrValue = 2, InvLoc = 0 };
+			Items tempItem2 = new Items { Name = "Test1", StrValue = 3, InvLoc = 0 };
+
+			myPlayer.GetItem(tempItem);
+			myPlayer.GetItem(tempItem2);
+
+			Assert.IsTrue(myPlayer.CurrentItems[0].Name == "Test1" && myPlayer.Str == 5, "GetItems has errors");
+		}
     }
 }
