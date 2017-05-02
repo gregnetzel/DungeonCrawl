9211a94a61a2de5caaa61f20c12063df1f0ffc1b
Alvin Nawa <alvinnawa11@gmail.com>
2017-05-02 10:27:42 -0700

    Add Item Test

---
+++ DungeonCrawl/UnitTestProject1/ItemsTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+namespace MobileApplication.Test
+{
+    [TestFixture()]
+    public class ItemsTest
+    {
+        [Test]
+        public void ItemConstructionTest()
+        {
+            Items testItem = new Items();
+            testItem.Name = "TestItem";
+            testItem.Type = "Dagger";
+            testItem.Description = "This is a TestItem";
+            testItem.StrValue = 5;
+            testItem.DexValue = 0;
+            testItem.SpdValue = 0;
+            testItem.HPValue = 0;
+            testItem.Image = "abc";
+            Assert.IsTrue(testItem.Name == "TestItem" &&
+                         testItem.Type == "Dagger" &&
+                         testItem.Description == "This is a TestItem" &&
+                          testItem.StrValue == 5 &&
+                          testItem.DexValue == 0 &&
+                          testItem.SpdValue == 0 &&
+                          testItem.HPValue == 0 &&
+                          testItem.Image == "abc"); 
+        }
+    }
+}
