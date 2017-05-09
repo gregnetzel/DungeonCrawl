8ae7d696faae09b1fd65b011da788d9e1f33c1ff
Alvin Nawa <alvinnawa11@gmail.com>
2017-05-09 08:57:02 -0700

    Removed DropItems from Monster because it’s never used. Dropped items are taken directly from the DB

--- DungeonCrawl/DungeonCrawl/DungeonCrawl/Models/Monster.cs
+++ DungeonCrawl/DungeonCrawl/DungeonCrawl/Models/Monster.cs
@@ -9,20 +9,7 @@ namespace DungeonCrawl.Models
     {
         public string Type { get; set; }
 
-        public Monster()
-        {
-            CurrentItems = new ObservableCollection<Item>();
-            for (int i = 0; i < 4; i++)
-            {
-                CurrentItems.Add(new Item());
-            }
-        }
-        // Creates a new Item and return it everytime it's killed
-        public Item DropItem()
-        {
-            Item ret = new Item { }; // This is going to be randomized when we already have the Items database
-            return ret;
-        }
+        public Monster() { }
 
         // Gives XP to battle after death
         public int DropXP()
