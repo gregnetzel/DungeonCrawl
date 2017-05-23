c725d1d43fe7b2b6b40d7f6c5e658764a53a3467
vinawan <alvinnawa11@gmail.com>
2017-05-23 11:54:04 -0700

    BattleEffect

---
+++ DungeonCrawl/DungeonCrawl/DungeonCrawl/Models/BattleEffects.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DungeonCrawl.Models
+{
+    class BattleEffects
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Target { get; set; } // {CHARACTERSINGLE, CHARACTERALL, MONSTERAALL, MONSTERSINGLE, ALL}
+        public string AttribMod { get; set; } // {STRENGTH, SPEED, DEFENSE, HP}
+        public int Tier { get; set; }
+    }
+}
